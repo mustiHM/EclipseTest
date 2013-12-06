48902d9f16d96f290a728a1a2b2e0184b798a549
mustiHM <qasmi@hm.edu>
2013-12-06 11:28:11 +0100

    Neue Server Implementierung für Queue-Mechanismus

---
+++ edu.hm.dako.wise1314 EchoApplication/src/main/java/edu/hm/dako/echo/server/QueueEchoServerImpl.java
@@ -0,0 +1,17 @@
+package edu.hm.dako.echo.server;
+
+public class QueueEchoServerImpl implements EchoServer {
+
+	@Override
+	public void start() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void stop() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
