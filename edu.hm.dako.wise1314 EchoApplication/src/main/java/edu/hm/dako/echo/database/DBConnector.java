d8d145fb0db641d91c7358c8ce5f249904ebcae1
mustiHM <qasmi@hm.edu>
2013-12-10 20:31:44 +0100

    Erweiterung der DB Verbindung mit Transaction Manager

--- edu.hm.dako.wise1314 EchoApplication/src/main/java/edu/hm/dako/echo/database/DBConnector.java
+++ edu.hm.dako.wise1314 EchoApplication/src/main/java/edu/hm/dako/echo/database/DBConnector.java
@@ -1,11 +1,16 @@
 package edu.hm.dako.echo.database;
 
-import javax.sql.XAConnection;
-import javax.transaction.xa.XAResource;
-import javax.transaction.xa.Xid;
+import javax.naming.InitialContext;
+import javax.sql.XADataSource;
 
-import com.mysql.jdbc.Connection;
+import org.enhydra.jdbc.standard.StandardXADataSource;
+import org.objectweb.jotm.Jotm;
+import org.objectweb.transaction.jta.TMService;
+
 import com.mysql.jdbc.jdbc2.optional.MysqlXADataSource;
+
+
+
 
 /**
  * Bietet Dienste zum Verbinden und Datenaustausch mit einer MySQL Datenbank über XA.
@@ -14,23 +19,41 @@ import com.mysql.jdbc.jdbc2.optional.MysqlXADataSource;
  */
 public class DBConnector {
 	
-	/**
-	 * Erstellt die Verbindung zu einer MySQL Datanbank über XA
-	 * @param userName der Benutzername
-	 * @param password das Passwort
-	 * @param databaseName der Name der Datenbank
-	 * @throws Exception Fehler beim Verbinden
-	 */
-	public DBConnector(String userName, String password, String databaseName) throws Exception{
-		MysqlXADataSource srcDB = new MysqlXADataSource();
-		srcDB.setUser(userName);
-		srcDB.setPassword(password);
-		srcDB.setDatabaseName(databaseName);
-		XAConnection srcXAConnection = srcDB.getXAConnection();
-		Connection con = (Connection) srcXAConnection.getConnection();
-		XAResource xaresource = srcXAConnection.getXAResource();
-		Xid xid1 = null;
-		xaresource.start(xid1, XAResource.TMNOFLAGS);
+	private String userName;
+	private String password;
+	private String databaseName;
+	private String url;
+    private TMService jotm;
+    private XADataSource xads;
+    private static final String USER_TRANSACTION_JNDI_NAME = "UserTransaction";
+	
+	public DBConnector(String userName, String password, String databaseName, String url) throws Exception{
+		this.userName = userName;
+		this.password = password;
+		this.databaseName = databaseName;
+		this.url = url;
+		
+		// Get a transction manager       
+        try {
+        	
+        	// creates an instance of JOTM with a local transaction factory which is not bound to a registry
+            jotm = new Jotm(true, false);
+            InitialContext ictx = new InitialContext();
+            ictx.rebind(USER_TRANSACTION_JNDI_NAME, jotm.getUserTransaction());
+            
+        } catch (Exception e){
+        	e.printStackTrace();
+        }
+        
+        xads = new StandardXADataSource();
+        ((StandardXADataSource) xads).setDriverName("org.gjt.mm.mysql.Driver");
+        // url in diesem Format: jdbc:mysql://localhost/javatest
+        ((StandardXADataSource) xads).setUrl(url);
+        ((StandardXADataSource) xads).setTransactionManager(jotm.getTransactionManager());
+        
+        // TODO weitere Impl.
 	}
 
+	
+	
 }
